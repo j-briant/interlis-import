855ae621239e70696ac824a1e27cbc87cf428615
j-briant <j.briant@protonmail.com>
2024-08-06 10:58:26 +0200

    better syntax

--- main.sh
+++ main.sh
@@ -35,39 +35,39 @@ done
 . .env
 
 # Get date.
-RUNDATE=$(date +"%Y%m%d")
-
 # Log folder variable
 LOGNAME=$(basename "$0")
 LOGFILE=$LOGPATH/$LOGNAME.log
 ERRORFILE=$LOGPATH/$LOGNAME.error
 
 # Get start time.
-echo START TIME: $(date +"%T") > $LOGFILE
+echo "START TIME: $(date +"%T")" > "$LOGFILE"
 
 # BACKUP
 if [ "$backup" = true ]; then
-        echo ===================================== BACKING UP DATABASE ===================================== >> $LOGFILE 2>&1
-        pg_dump $DATABASE -n $SCHEMA -Fc > backup.dmp >> $LOGFILE 2>&1
+        echo "===================================== BACKING UP DATABASE =====================================" >> "$LOGFILE" 2>&1
+        pg_dump "$DATABASE" -n "$SCHEMA" -Fc > backup.dmp >> "$LOGFILE" 2>&1
 fi
 
 # IMPORT INTERLIS FILES.
-echo ===================================== CREATE SCHEMAS ===================================== >> $LOGFILE 2>&1
-echo Creating schema... >> $LOGFILE 2>&1
-src/create_schema.sh -U $USER -p $PORT -H $HOST -s $SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -i $INTERLISMODELFILE -m $MODEL -E -T -B >> $LOGFILE 2>&1
+{
+    echo "===================================== CREATE SCHEMAS ====================================="
+    echo "Creating schema..."
+    src/create_schema.sh -U "$USER" -p "$PORT" -H "$HOST" -s "$SCHEMA" -d "$DATABASE" -w "$PASSWORD" -n "$T_ID_NAME" -i "$INTERLISMODELFILE" -m "$MODEL" -E -T -B
 
-echo ===================================== IMPORT DATA ===================================== >> $LOGFILE 2>&1
-echo Importing data from Interlis files... >> $LOGFILE 2>&1
-src/import_itf.sh -U $USER -p $PORT -H $HOST -s $SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -f $INTERLISDATAFILE >> $LOGFILE 2>&1
+    echo "===================================== IMPORT DATA ====================================="
+    echo "Importing data from Interlis files..."
+    src/import_itf.sh -U "$USER" -p "$PORT" -H "$HOST" -s "$SCHEMA" -d "$DATABASE" -w "$PASSWORD" -n "$T_ID_NAME" -f "$INTERLISDATAFILE"
+} >> "$LOGFILE" 2>&1
 
 # Get end time.
-echo END TIME: $(date +"%T") >> $LOGFILE
+echo "END TIME: $(date +"%T")" >> "$LOGFILE"
 
 # Copy error from LOGFILE into ERRORFILE
-grep -n -i error $LOGFILE > $ERRORFILE 
+grep -n -i error "$LOGFILE" > "$ERRORFILE" 
 
 # Set exit status depending on $ERRORFILE
-if [ -s $ERRORFILE ]; then
+if [ -s "$ERRORFILE" ]; then
         # The file is not-empty
 	exit 1
 else
