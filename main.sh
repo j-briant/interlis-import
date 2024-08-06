8ae3ce8135bec6ccedf24fe1e097d3530b4d270b
sgca0260 <julien.briant@lausanne.ch>
2024-08-06 10:25:19 +0200

    cleaning

--- main.sh
+++ main.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/bash
 
-# This script is the main script for updating MO data and generating shapefiles for the guichet. Processes are:
-#	1. Backuping the database
-#	2. Downloading Interlis files from the ASIT-VD viageo URL
-#	3. Creating the Postgres schema from the Interlis file model
-#	4. Importing downloaded Interlis files into the newly created schema
-#	5. Importing Goeland data from the goeland database
-#	6. Count objects in tables and views
-#	7. Send email notification
+# This script is the main script for updating data from interlis. Processes are:
+#	1. Backing-up the database if desired
+#	2. Creating the Postgres schema from the Interlis file model
+#	3. Importing downloaded Interlis files into the newly created schema
 
+usage="$(basename "$0") [-h] [-b]
+Create the model structure into a schema and import data from interlis:
+	-h show this help text
+	-b backup database schema before anything (must run with backup privilege)"
 
-# Get configuration variables
-. /etc/update_guichet/paths.conf
+# Get parameters.
+while :; do
+	case $1 in
+		-h|-\?|--help) 
+			echo "$usage" 
+			exit;;
+		-b|--backup) 
+			backup=true;;
+		--)
+			shift
+			break
+			;;
+		-?*)
+			printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+			;;
+		*)
+			break
+	esac
+	shift
+done
 
 # Get environment variables.
-. $CONFPATH/.env
-
-MO_COMMUNES=$CONFPATH/mo_communes.json
-NPCS_COMMUNES=$CONFPATH/npcs_communes.json
-MOVD_MODEL=$CONFPATH/model/6021.ili
-TMPEXPORT=/tmp/guichet/
+. .env
 
 # Get date.
 RUNDATE=$(date +"%Y%m%d")
@@ -28,57 +41,24 @@ RUNDATE=$(date +"%Y%m%d")
 LOGNAME=$(basename "$0")
 LOGFILE=$LOGPATH/$LOGNAME.log
 ERRORFILE=$LOGPATH/$LOGNAME.error
-RUNTIMEFILE=$LOGPATH/$LOGNAME.runtime
-
-# Create folder if not exists
-mkdir -p $LOGPATH
-mkdir -p $TMPEXPORT
-# Change owner to postgres so they can write
-chown postgres $TMPEXPORT
 
 # Get start time.
 echo START TIME: $(date +"%T") > $LOGFILE
 
+# BACKUP
+if [ "$backup" = true ]; then
+        echo ===================================== BACKING UP DATABASE ===================================== >> $LOGFILE 2>&1
+        pg_dump $DATABASE -n $SCHEMA -Fc > backup.dmp >> $LOGFILE 2>&1
+fi
 
-# DOWNLOAD AND IMPORT INTERLIS FILES FROM AVRIC.
-echo ===================================== BACKING UP DATABASE ===================================== >> $LOGFILE 2>&1
-su - postgres -c "pg_dump $DATABASE -n $MOVD_SCHEMA -n $NPCSVD_SCHEMA -Fc > /data/backups/mensuration_officielle.backup" >> $LOGFILE 2>&1
-
-
-echo ===================================== DOWNLOADS ===================================== >> $LOGFILE 2>&1
-echo Downloading MO files... >> $LOGFILE 2>&1
-$SCRIPTPATH/src/download_itf.sh -a "$AUTHORIZATION" -c $MO_COMMUNES -l $MOVD_DOWNLOAD_LINK -f $MOVD_DOWNLOADPATH >> $LOGFILE 2>&1
-
-echo Downloading NPCS files... >> $LOGFILE 2>&1
-$SCRIPTPATH/src/download_itf.sh -a "$AUTHORIZATION" -c $NPCS_COMMUNES -l $NPCSVD_DOWNLOAD_LINK -f $NPCSVD_DOWNLOADPATH >> $LOGFILE 2>&1
-
-
+# IMPORT INTERLIS FILES.
 echo ===================================== CREATE SCHEMAS ===================================== >> $LOGFILE 2>&1
-echo Creating MO schema... >> $LOGFILE 2>&1
-$SCRIPTPATH/src/create_schema.sh -U $USER -p $PORT -H $HOST -s $MOVD_SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -m $MOVD_MODEL -E -T -B >> $LOGFILE 2>&1
-
-echo Creating NPCS schema... >> $LOGFILE 2>&1
-$SCRIPTPATH/src/create_schema.sh -U $USER -p $PORT -H $HOST -s $NPCSVD_SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -m $MOVD_MODEL -E -T -B >> $LOGFILE 2>&1
-
+echo Creating schema... >> $LOGFILE 2>&1
+src/create_schema.sh -U $USER -p $PORT -H $HOST -s $SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -i $INTERLISMODELFILE -m $MODEL -E -T -B >> $LOGFILE 2>&1
 
 echo ===================================== IMPORT DATA ===================================== >> $LOGFILE 2>&1
-echo Importing MO data from Interlis files... >> $LOGFILE 2>&1
-$SCRIPTPATH/src/import_itf.sh -U $USER -p $PORT -H $HOST -s $MOVD_SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -f $MOVD_DOWNLOADPATH >> $LOGFILE 2>&1
-
-echo Importing NPCS data from Interlis files... >> $LOGFILE 2>&1
-$SCRIPTPATH/src/import_itf.sh -U $USER -p $PORT -H $HOST -s $NPCSVD_SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -f $NPCSVD_DOWNLOADPATH >> $LOGFILE 2>&1
-
-
-# COUNT OBJECTS FOR CONTROL
-echo ===================================== COUNTING TABLES OBJECTS ===================================== >> $LOGFILE 2>&1
-echo Counting MO objects... >> $LOGFILE 2>&1
-su - postgres -c "psql -d $DATABASE -c \"select db_monitoring.count_table_object('$MOVD_SCHEMA');\"" >> $LOGFILE 2>&1
-echo Counting NPCS objects... >> $LOGFILE 2>&1
-su - postgres -c "psql -d $DATABASE -c \"select db_monitoring.count_table_object('$NPCSVD_SCHEMA');\"" >> $LOGFILE 2>&1
-echo Counting Lausanne data objects... >> $LOGFILE 2>&1
-su - postgres -c "psql -d $DATABASE -c \"select db_monitoring.count_table_object('specificite_lausanne');\"" >> $LOGFILE 2>&1
-echo Counting diffusion objects... >> $LOGFILE 2>&1
-su - postgres -c "psql -d diffusion -c \"select db_monitoring.count_table_object('mo_guichet');\"" >> $LOGFILE 2>&1
+echo Importing data from Interlis files... >> $LOGFILE 2>&1
+src/import_itf.sh -U $USER -p $PORT -H $HOST -s $SCHEMA -d $DATABASE -w $PASSWORD -n $T_ID_NAME -f $INTERLISDATAFILE >> $LOGFILE 2>&1
 
 # Get end time.
 echo END TIME: $(date +"%T") >> $LOGFILE
--- src/create_schema.sh
+++ src/create_schema.sh
@@ -2,7 +2,7 @@
 
 # CREATE THE DATABASE SCHEMA
 
-usage="$(basename "$0") [-h] [-U USER] [-H HOST] [-p PORT] [-s SCHEMA] [-d DATABASE] [-w PASSWORD] [-E ENUMTAB] [-T TIDCOL] [-B BASKETCOL] [-n TIDNAME] [-m INTERLISMODEL]
+usage="$(basename "$0") [-h] [-U USER] [-H HOST] [-p PORT] [-s SCHEMA] [-d DATABASE] [-w PASSWORD] [-E ENUMTAB] [-T TIDCOL] [-B BASKETCOL] [-n TIDNAME] [-i INTERLISFILE] [-m INTERLISMODEL]
 Create a Postgres schema based on an interlis model (.ili):
 	-h show this help text
 	-U database user
@@ -15,20 +15,21 @@ Create a Postgres schema based on an interlis model (.ili):
 	-T --createTidCol, keep blank if no
 	-B --createBasketCol, keep blank if no
 	-n t_id column name
-	-m interlis model path"
+	-m interlis model"
 
 # Set parameters
-while getopts :hU:H:p:s:d:w:n:m:ETB flag
+while getopts :hU:H:p:s:d:w:n:i:m:ETB flag
 do
         case "${flag}" in
 		h) echo "$usage"; exit;;
 		U) user=${OPTARG};;
-                H) host=${OPTARG};;
-                p) port=${OPTARG};;
-                s) schema=${OPTARG};;
+        H) host=${OPTARG};;
+        p) port=${OPTARG};;
+        s) schema=${OPTARG};;
 		d) database=${OPTARG};;
 		w) password=${OPTARG};;
 		n) tidname=${OPTARG};;
+		i) interlisfile=${OPTARG};;
 		m) interlismodel=${OPTARG};;
 		E) enumtab="--createEnumTabs";;
 		T) tidcol="--createTidCol";;
@@ -40,11 +41,11 @@ done
 
 
 # Make parameters mandatory
-if [ ! ${user} ] || [ ! ${host} ] || [ ! ${port} ] || [ ! ${schema} ] || [ ! ${database} ] || [ ! ${password} ] || [ ! ${tidname} ] || [ ! ${interlismodel} ]; then
-  echo "arguments -U, -h, -p, -s, -d, -w, -n and -m must be provided"
+if [ ! ${user} ] || [ ! ${host} ] || [ ! ${port} ] || [ ! ${schema} ] || [ ! ${database} ] || [ ! ${password} ] || [ ! ${tidname} ] || [ ! ${interlisfile} ] || [ ! ${interlismodel} ]; then
+  echo "arguments -U, -h, -p, -s, -d, -w, -n, -i and -m must be provided"
   echo "$usage" >&2; exit 1
 fi
 
 
 # Run ili2db and create the schema.
-java -jar "/opt/ili2pg-5.0.1/ili2pg-5.0.1.jar" --schemaimport --dbusr $user --dbhost $host --dbport $port --dbschema $schema --dbdatabase $database --dbpwd $password --sqlEnableNull --coalesceCatalogueRef --createEnumTabs --createNumChecks --createFk --createFkIdx --coalesceMultiSurface --coalesceMultiLine --coalesceMultiPoint --coalesceArray --beautifyEnumDispName --createGeomIdx --createMetaInfo --expandMultilingual --createTypeConstraint $tidcol --t_id_Name $tidname $enumtab $basketcol --importTid --smart2Inheritance --defaultSrsCode 2056 --models MD01MOVDMN95V24 $interlismodel 2>&1
+java -jar "/opt/ili2pg-5.0.1/ili2pg-5.0.1.jar" --schemaimport --dbusr $user --dbhost $host --dbport $port --dbschema $schema --dbdatabase $database --dbpwd $password --sqlEnableNull --coalesceCatalogueRef --createEnumTabs --createNumChecks --createFk --createFkIdx --coalesceMultiSurface --coalesceMultiLine --coalesceMultiPoint --coalesceArray --beautifyEnumDispName --createGeomIdx --createMetaInfo --expandMultilingual --createTypeConstraint $tidcol --t_id_Name $tidname $enumtab $basketcol --importTid --smart2Inheritance --defaultSrsCode 2056 --models $interlismodel $interlisfile 2>&1
